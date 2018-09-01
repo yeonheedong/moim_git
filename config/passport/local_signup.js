3ac5e9d72247d6f666944fe39e0b7a9b6eed9fe3
KimCheolHwan <jasd0330@naver.com>
2018-09-01 15:59:13 +0900

    signup

--- config/passport/local_signup.js
+++ config/passport/local_signup.js
@@ -1,6 +1,6 @@
 /**
  * 패스포트 설정 파일
- * 
+ *
  * 로컬 인증방식에서 회원가입에 사용되는 패스포트 설정
  *
  * @date 2016-11-10
@@ -8,6 +8,7 @@
  */
 
 var LocalStrategy = require('passport-local').Strategy;
+var eth = require('../../dapp/eth.js');
 
 module.exports = new LocalStrategy({
 		usernameField : 'email',
@@ -16,9 +17,9 @@ module.exports = new LocalStrategy({
 	}, function(req, email, password, done) {
         // 요청 파라미터 중 name 파라미터 확인
         var paramName = req.body.name || req.query.name;
-	 
+
 		console.log('passport의 local-signup 호출됨 : ' + email + ', ' + password + ', ' + paramName);
-		
+
 	    // findOne 메소드가 blocking되지 않도록 하고 싶은 경우, async 방식으로 변경
 	    process.nextTick(function() {
 	    	var database = req.app.get('database');
@@ -27,24 +28,25 @@ module.exports = new LocalStrategy({
 		        if (err) {
 		            return done(err);
 		        }
-		        
+
 		        // 기존에 사용자 정보가 있는 경우
 		        if (user) {
 		        	console.log('기존에 계정이 있음.');
 		            return done(null, false, req.flash('signupMessage', '계정이 이미 있습니다.'));  // 검증 콜백에서 두 번째 파라미터의 값을 false로 하여 인증 실패한 것으로 처리
 		        } else {
 		        	// 모델 인스턴스 객체 만들어 저장
-		        	var user = new database.UserModel({'email':email, 'password':password, 'name':paramName});
+							var address = eth.addressCreate(password);
+		        	var user = new database.UserModel({'email':email, 'password':password, 'name':paramName, 'address':address});
 		        	user.save(function(err) {
 		        		if (err) {
 		        			throw err;
 		        		}
-		        		
+
 		        	    console.log("사용자 데이터 추가함.");
 		        	    return done(null, user);  // 검증 콜백에서 두 번째 파라미터의 값을 user 객체로 넣어 인증 성공한 것으로 처리
 		        	});
 		        }
-		    });    
+		    });
 	    });
 
 	});
